b8b4177487ca8b040dfdfbf8ece18cd94247639d
edo979 <prezentabilan@gmail.com>
2022-08-31 22:42:44 +0200

    add js file, refactor data attribute

---
+++ script.js
@@ -0,0 +1,8 @@
+const btnEls = document.getElementById('buttons')
+
+btnEls.addEventListener('click', (e) => {
+  /** @type {Element} */
+  const el = e.target.closest('[data-btn-icon]')
+
+  el.classList.add('move-to-user-select')
+})
