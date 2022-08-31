b2a3ca1219c13a251038ac8e0606f798db8caa2a
edo979 <prezentabilan@gmail.com>
2022-08-31 23:35:23 +0200

    hide and remove not selected

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const btnEls = document.getElementById('buttons')
+const btnEls = document.getElementById('buttons'),
+  bgImgEl = document.getElementById('bg-image')
 
 btnEls.addEventListener('click', (e) => {
   /** @type {Element} */
   const el = e.target.closest('[data-btn-icon]')
 
   el.classList.add('move-to-house-select')
+  btnEls.classList.add('selected-visible')
+})
+
+bgImgEl.addEventListener('transitionend', function (e) {
+  btnEls.classList.add('remove-children-after-transition')
 })
