2a9f0beb8a9b30d97f17358af788e1ca226d5aab
edo979 <prezentabilan@gmail.com>
2022-09-01 17:31:11 +0200

    refactor js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const btnEls = document.getElementById('buttons'),
+const gameEl = document.getElementById('game-container'),
   bgImgEl = document.getElementById('bg-image')
 
-btnEls.addEventListener('click', (e) => {
+gameEl.addEventListener('click', (e) => {
   /** @type {Element} */
   const el = e.target.closest('[data-btn-icon]')
 
@@ -9,17 +9,17 @@ btnEls.addEventListener('click', (e) => {
   el.classList.add('move-to-user-select')
 
   // Add class to house selected
-  btnEls
+  gameEl
     .querySelector('[data-btn-icon="rock"]')
     .classList.add('move-to-house-select')
 
   // Hide elements
-  btnEls.classList.add('hide-elements')
+  gameEl.classList.add('hide-elements')
 
   // Move to step 2
-  btnEls.classList.add('step-2')
+  gameEl.classList.add('step-2')
 })
 
 bgImgEl.addEventListener('transitionend', function (e) {
-  btnEls.classList.add('remove-children-after-transition')
+  gameEl.classList.add('remove-children-after-transition')
 })
