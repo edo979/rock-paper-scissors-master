6b62c5ed8075eaf692c884a00530b11031c6ece1
edo979 <prezentabilan@gmail.com>
2022-09-01 17:17:09 +0200

    add game result

--- script.js
+++ script.js
@@ -5,8 +5,19 @@ btnEls.addEventListener('click', (e) => {
   /** @type {Element} */
   const el = e.target.closest('[data-btn-icon]')
 
-  el.classList.add('move-to-house-select')
+  // Add class to user selecet
+  el.classList.add('move-to-user-select')
+
+  // Add class to house selected
+  btnEls
+    .querySelector('[data-btn-icon="rock"]')
+    .classList.add('move-to-house-select')
+
+  // Hide elements
   btnEls.classList.add('hide-elements')
+
+  // Move to step 2
+  btnEls.classList.add('step-2')
 })
 
 bgImgEl.addEventListener('transitionend', function (e) {
