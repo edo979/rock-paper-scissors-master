49ef7de07fd6813280183f2875f527724cfe4d4b
edo979 <prezentabilan@gmail.com>
2022-09-05 11:43:05 +0200

    fix element visibility, fix outline button position, add to js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const gameEl = document.getElementById('game-container'),
   bgImgEl = document.getElementById('bg-image'),
   state = {
+    picks: ['rock', 'paper', 'scissors'],
+    userPick: undefined,
+    housePick: undefined,
+    score: 0,
     userSelectEl: undefined,
   }
 
@@ -8,17 +12,19 @@ gameEl.addEventListener('click', (e) => {
   /** @type {Element} */
   const el = e.target.closest('[data-btn-icon]')
   state.userSelectEl = el
+  console.log(el)
+  console.log(el.dataset.btnIcon)
 
   // Add class to user selecet
-  el.classList.add('move-to-user-select')
+  //el.classList.add('move-to-user-select')
 
   // Add class to house selected
-  gameEl
-    .querySelector('[data-btn-icon="rock"]')
-    .classList.add('move-to-house-select')
+  // gameEl
+  //   .querySelector('[data-btn-icon="rock"]')
+  //   .classList.add('move-to-house-select')
 
-  // Move to step 2
-  gameEl.classList.add('step-2')
+  // // Move to step 2
+  // gameEl.classList.add('step-2')
 })
 
 bgImgEl.addEventListener('transitionend', function (e) {
