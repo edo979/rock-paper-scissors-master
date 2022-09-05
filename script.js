7bfa2db85250efd6338b5604721a6eb1b989874d
edo979 <prezentabilan@gmail.com>
2022-09-05 12:22:24 +0200

    add class to winer pick

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const gameEl = document.getElementById('game-container'),
   bgImgEl = document.getElementById('bg-image'),
   state = {
+    PAPPER: 'paper',
+    ROCK: 'rock',
+    SCISSORS: 'scissors',
     picks: ['rock', 'paper', 'scissors'],
     userPick: undefined,
     housePick: undefined,
+    isUserWiner: false,
     score: 0,
-    userSelectEl: undefined,
   }
 
 gameEl.addEventListener('click', (e) => {
   /** @type {Element} */
   state.userPick = e.target.closest('[data-btn-icon]').dataset.btnIcon
   state.housePick = getHousePick()
-  console.log(state)
 
   // Add class to user selecet
   gameEl
@@ -24,6 +26,9 @@ gameEl.addEventListener('click', (e) => {
     .querySelector(`[data-btn-icon="${state.housePick}"]`)
     .classList.add('move-to-house-select')
 
+  // Winer is...
+  state.isUserWiner = getIsUserWiner()
+
   // // Move to step 2
   gameEl.classList.add('step-2')
 })
@@ -31,9 +36,16 @@ gameEl.addEventListener('click', (e) => {
 bgImgEl.addEventListener('transitionend', function (e) {
   setTimeout(() => {
     gameEl.classList.add('step-3')
-    gameEl
-      .querySelector(`[data-btn-icon="${state.userPick}"]`)
-      .classList.add('radial-bg')
+
+    let winerEl = undefined
+
+    if (state.isUserWiner) {
+      winerEl = gameEl.querySelector(`[data-btn-icon="${state.userPick}"]`)
+    } else {
+      winerEl = gameEl.querySelector(`[data-btn-icon="${state.housePick}"]`)
+    }
+
+    winerEl.classList.add('radial-bg')
   }, 1000)
 })
 
@@ -42,4 +54,31 @@ function getHousePick() {
   const randomNum = Math.floor(Math.random() * 2)
 
   return availablePicks[randomNum]
+}
+
+function getIsUserWiner() {
+  const { PAPPER, ROCK, SCISSORS, userPick, housePick } = state
+
+  switch (userPick) {
+    case PAPPER:
+      if (housePick === ROCK) {
+        return true
+      }
+      return false
+
+    case ROCK:
+      if (housePick === SCISSORS) {
+        return true
+      }
+      return false
+
+    case SCISSORS:
+      if (housePick === PAPPER) {
+        return true
+      }
+      return false
+
+    default:
+      return false
+  }
 }
