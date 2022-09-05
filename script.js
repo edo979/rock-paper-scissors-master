ff1306b48b0c7f278e722e79c4431368e18e1002
edo979 <prezentabilan@gmail.com>
2022-09-05 12:03:48 +0200

    add class to selected pick

--- script.js
+++ script.js
@@ -10,26 +10,36 @@ const gameEl = document.getElementById('game-container'),
 
 gameEl.addEventListener('click', (e) => {
   /** @type {Element} */
-  const el = e.target.closest('[data-btn-icon]')
-  state.userSelectEl = el
-  console.log(el)
-  console.log(el.dataset.btnIcon)
+  state.userPick = e.target.closest('[data-btn-icon]').dataset.btnIcon
+  state.housePick = getHousePick()
+  console.log(state)
 
   // Add class to user selecet
-  //el.classList.add('move-to-user-select')
+  gameEl
+    .querySelector(`[data-btn-icon="${state.userPick}"]`)
+    .classList.add('move-to-user-select')
 
   // Add class to house selected
-  // gameEl
-  //   .querySelector('[data-btn-icon="rock"]')
-  //   .classList.add('move-to-house-select')
+  gameEl
+    .querySelector(`[data-btn-icon="${state.housePick}"]`)
+    .classList.add('move-to-house-select')
 
   // // Move to step 2
-  // gameEl.classList.add('step-2')
+  gameEl.classList.add('step-2')
 })
 
 bgImgEl.addEventListener('transitionend', function (e) {
   setTimeout(() => {
     gameEl.classList.add('step-3')
-    state.userSelectEl.classList.add('radial-bg')
+    gameEl
+      .querySelector(`[data-btn-icon="${state.userPick}"]`)
+      .classList.add('radial-bg')
   }, 1000)
 })
+
+function getHousePick() {
+  const availablePicks = state.picks.filter((pik) => pik !== state.userPick)
+  const randomNum = Math.floor(Math.random() * 2)
+
+  return availablePicks[randomNum]
+}
